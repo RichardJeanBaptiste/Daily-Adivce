b118a0904406a7db090cee34939dde3966389dc6
Richard Jean-Baptiste <Richinbk1@gmail.com>
2023-07-05 20:50:04 -0400

    Updates

---
+++ precache-manifest.fc7297959ee2c0bc2d2a09f49744daea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46dda75821cc237118d4449f38032266",
+    "url": "/daily/index.html"
+  },
+  {
+    "revision": "1efcea80acd96ce4dd87",
+    "url": "/daily/static/css/main.9e481b50.chunk.css"
+  },
+  {
+    "revision": "152e18702e3aa6e0c2b0",
+    "url": "/daily/static/js/2.7321433e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/daily/static/js/2.7321433e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efcea80acd96ce4dd87",
+    "url": "/daily/static/js/main.cfcf02ff.chunk.js"
+  },
+  {
+    "revision": "e35ec0b93d220ad887d3",
+    "url": "/daily/static/js/runtime-main.62a8e4b1.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/daily/static/media/city.91c109fc.jpg"
+  }
+]);
